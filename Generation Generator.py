8e88227b43392f0f4395df06f470e6ca67ebbf91
Dee <dom.aves@gmail.com>
2024-09-19 14:43:51 +0100

    Create Generation Generator.py
    Day 9 of 100 days

---
+++ Generation Generator.py
@@ -0,0 +1,16 @@
+# Generation Generator
+birth_year = int(input("What year where you born?"))
+if birth_year >= 1925 and birth_year <= 1946:
+  print("Traditionalists")
+elif birth_year >= 1947 and birth_year <= 1964:
+  print(" Baby Boomers")
+elif birth_year >= 1965 and birth_year <= 1981:
+  print("Generation X")
+elif birth_year >= 1982 and birth_year <= 1995:
+  print("Millenials")
+elif birth_year >= 1996 and birth_year <= 2015:
+ print("Generation Z")
+elif birth_year >= 2016 and birth_year <= 2024:
+  print("Generation Alpha")
+if birth_year >= 2025 and birth_year <= 2039:
+ print("Generation Beta")
